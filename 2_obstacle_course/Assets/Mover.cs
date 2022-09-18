4a0af8af4a376ea4aa50193ce5db903751d6210b
ybuser <ybuser@naver.com>
2022-09-19 00:45:31 +0900

    video 12 fin

--- 2_obstacle_course/Assets/Mover.cs
+++ 2_obstacle_course/Assets/Mover.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    [SerializeField] float xValue = 0;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f,0,0);
+        transform.Translate(xValue,yValue,zValue);
     }
 }
